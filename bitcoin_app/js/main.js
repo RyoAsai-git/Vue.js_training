91b7f667552c09f75b0728ab58602f918243317f
浅井玲 <fu.orangina0516@docomo.ne.jp>
2021-05-02 21:43:06 +0900

    ローディング画面作成

--- bitcoin_app/js/main.js
+++ bitcoin_app/js/main.js
@@ -3,17 +3,21 @@ var app = new Vue({
   data: {
     bpi: null,
     hasError: false,
+    loading: true
   },
   mounted: function() {
     axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
     .then(function(response){
-      console.log(response);
-      console.log(response.data.bpi.USD.rate_float)
+      // console.log(response);
+      // console.log(response.data.bpi.USD.rate_float)
       this.bpi = response.data.bpi
     }.bind(this))
-    .catch(function(error) {
+    .catch(function(error){
       console.log(error)
       this.hasError = true
+    }.bind(this))
+    .finally(function(){
+      this.loading = false
     }.bind(this))
   },
   filters: {
