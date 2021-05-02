c971530b79b4d37ce877ff5c2d4518a3b3f8212b
浅井玲 <fu.orangina0516@docomo.ne.jp>
2021-05-02 21:13:52 +0900

    API値取得 通信エラー時内容出力

--- bitcoin_app/js/main.js
+++ bitcoin_app/js/main.js
@@ -4,7 +4,15 @@ var app = new Vue({
     bpi: null,
   },
   mounted: function() {
-    
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(function(response){
+      // console.log(response);
+      // console.log(response.data.bpi.USD.rate_float)
+      this.bpi = response.data.bpi
+    }.bind(this))
+    .catch(function(error) {
+      console.log(error)
+    })
   }
 
 })
