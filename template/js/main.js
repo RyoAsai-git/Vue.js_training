6d8659c41245cf1a0b02365749e4e57de8caf962
浅井玲 <fu.orangina0516@docomo.ne.jp>
2021-05-03 21:04:26 +0900

    ローカルフィルタ

--- template/js/main.js
+++ template/js/main.js
@@ -5,10 +5,16 @@ var app = new Vue ({
     message2: 'Hello <span style="color:red">Vue.js</span>',
     number: 100,
     ok: true,
+    price: 29800,
   },
   methods: {
     clickHandler: function(event) {
       this.message = this.message.split('').reverse().join('')
+    }
+  },
+  filters: {
+    numberFormat: function(value) {
+      return value.toLocaleString();
     }
   }
 })
