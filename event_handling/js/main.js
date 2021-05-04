f8fe174a605166fefe6427b942d22de517fef100
浅井玲 <fu.orangina0516@docomo.ne.jp>
2021-05-04 22:10:45 +0900

    ªブジェクト 引数で渡す

--- event_handling/js/main.js
+++ event_handling/js/main.js
@@ -2,10 +2,16 @@ var app = new Vue({
   el: '#app',
   data: {
     counter: 0,
+    message: '',
   },
   methods: {
-    clickHandler: function() {
+    clickHandler: function($event, message) {
       this.counter++
+      this.message = message
+      console.log($event)
+      console.log($event.target.tagName)
+      console.log($event.target.innerHTML)
+      console.log($event.target.type)
     }
   }
 })
