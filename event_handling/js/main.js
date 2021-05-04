26fdf011ce0cc4ccf6c55f4e451dbcde0012344b
浅井玲 <fu.orangina0516@docomo.ne.jp>
2021-05-04 22:14:27 +0900

    イベント修飾子.once

--- event_handling/js/main.js
+++ event_handling/js/main.js
@@ -5,13 +5,16 @@ var app = new Vue({
     message: '',
   },
   methods: {
-    clickHandler: function($event, message) {
-      this.counter++
-      this.message = message
-      console.log($event)
-      console.log($event.target.tagName)
-      console.log($event.target.innerHTML)
-      console.log($event.target.type)
+    // clickHandler: function($event, message) {
+    //   this.counter++
+    //   this.message = message
+    //   console.log($event)
+    //   console.log($event.target.tagName)
+    //   console.log($event.target.innerHTML)
+    //   console.log($event.target.type)
+    // }
+    clickHandler: function() {
+      this.message = new Date().toLocaleTimeString()
     }
   }
 })
