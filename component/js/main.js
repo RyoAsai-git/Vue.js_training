9686d27afa054da307184dd8a9d8691355e10682
浅井玲 <fu.orangina0516@docomo.ne.jp>
2021-05-05 23:39:43 +0900

    コンポーネント オプション詳細

--- component/js/main.js
+++ component/js/main.js
@@ -3,6 +3,21 @@ Vue.component('global-component', {
   template: '<p>global</p>'
 })
 
+Vue.component('button-counter', {
+  // コンポーネントのデータは関数である必要がある
+  //各インスタンスが独自のインスタンスを利用できるようにするため
+  data: function() {
+    return {
+      count: 0
+    }
+  },
+  // template: '<button v-on:click="count++">{{ count }}</button>'
+  
+  //以下はエラー 要素はdivなどで囲む必要がある
+  // template: '</span><button v-on:click="count++">{{ count }}</button>'
+  template: '<div><span>count: </span><button v-on:click="count++">{{ count }}</button></div>'
+})
+
 var localComponent = {
   template: '<p>local</p>'
 }
